97d0b3875c092e99c56016d33f22ab98355f5fcc
FORTE <fortegp05@hotmail.com>
2020-11-08 11:39:43 +0900

    Updates

---
+++ precache-manifest.f595369d4092aec913367991998c3ec9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6266b0e0236ecbe4c34176cf10222df6",
+    "url": "/portfolio_react/index.html"
+  },
+  {
+    "revision": "9c83394cd3c4f4494586",
+    "url": "/portfolio_react/static/css/main.393d4cb9.chunk.css"
+  },
+  {
+    "revision": "7ece2ba126b768c0292a",
+    "url": "/portfolio_react/static/js/2.ad51537d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/portfolio_react/static/js/2.ad51537d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c83394cd3c4f4494586",
+    "url": "/portfolio_react/static/js/main.7a274762.chunk.js"
+  },
+  {
+    "revision": "31d5ba04d1fafd9a5a35",
+    "url": "/portfolio_react/static/js/runtime-main.17440bfd.js"
+  }
+]);
